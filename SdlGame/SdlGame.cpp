d8ac0081ab99a09d34bab304a8a620514109db09
Stefan Iancu <stefan.v.iancu@gmail.com>
2023-05-06 22:00:53 +0300

    Making dwaagons vs Beh the moth

--- SdlGame/SdlGame.cpp
+++ SdlGame/SdlGame.cpp
@@ -2,29 +2,48 @@
 #include <random>
 #include <ctime>
 using namespace std;
-
+const int BEH_HEALTH = 500;
+const int DRAG_HEALTH = 500;
+const int BEH_ATTACK = 10;
+const int DRAGON_ATTACK = 15;
 int main()
 {
 	default_random_engine randomGenerator(time(NULL));
-	uniform_real_distribution<float> atticRoll(0.0f, 1.0f);
-	int health = 100;
-	int cnt = 0;
-	while (true) {
-		float attack = atticRoll(randomGenerator);
-		if (attack > 0.3f) {
-			cout << "I misseed the snake oh noooooooooooooooooooooooooooooooooooooooooooo\n";
+	uniform_int_distribution<int> DragonRoll(0,DRAGON_ATTACK);
+	uniform_int_distribution<int> BehemothRoll(0,BEH_ATTACK);
+	int DragonCnt, BehCnt;
+	cout << "How many dragons are there? ";
+	cin >> DragonCnt;
+	cout << "\nHow many behemots are there? ";
+	cin >> BehCnt;
+	int dhealth = DRAG_HEALTH;
+	int bhealth = BEH_HEALTH;
+//	cout << "\n *Battle Noises*\n";
+	while (DragonCnt>0&&BehCnt>0) {
+		bhealth -=DragonRoll(randomGenerator);
+		//cout << " Beh has " << bhealth << " health left.\n";
+		if (bhealth <= 0) {
+			BehCnt--;
+			bhealth = BEH_HEALTH;
+		//	cout << "Beth died.\n";
 		}
-		else {
-			cout << "I hit the snake yassssssssssssssssssssssssssssssssssssssss.\n";
-			health -= 10;
-		}
-		cnt++;
-		if (health == 0) {
-			cout << "Dah snek is ded\n";
-			cout << "Took you " << cnt << " hits ya loser. Get better dexterity.";
-			break;
+		
+		dhealth -=BehemothRoll(randomGenerator);
+	//	cout << " Dragon has " << dhealth << " health left.\n";
+		if (dhealth <= 0) {
+			DragonCnt--;
+			dhealth = DRAG_HEALTH;
+		///	cout << "Dwagon died :c \n";
 		}
 	}
+	if (DragonCnt < BehCnt) {
+		cout << "Behemoths won!\n";
+	}
+	else {
+		cout << "Dragons won!\n";
+	}
+	cout << "There are " << DragonCnt << " dragons left and " << BehCnt << " behemoths left.\n";
+
 
 }
 
