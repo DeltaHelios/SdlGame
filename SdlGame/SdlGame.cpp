8d824250a13c15dcc4604ff382108bd8beca72b6
Stefan Iancu <stefan.v.iancu@gmail.com>
2023-05-06 21:14:49 +0300

    Removing comment

--- SdlGame/SdlGame.cpp
+++ SdlGame/SdlGame.cpp
@@ -5,29 +5,26 @@ using namespace std;
 
 int main()
 {
-    default_random_engine randomGenerator(time(NULL));
-    uniform_real_distribution<float> AtticRoll(0.0f,1.0f);
-    /*for (int i = 0; i < 10; i++) {
-
-    cout << "I rolled a "<<AtticRoll(randomGenerator)<<endl;
-    }*/
-    int health=100;
-        int cnt = 0;
-    while (true) {
-    float attack = AtticRoll(randomGenerator);
-    if (attack > 0.3f) {
-        cout << "I misseed the snake oh noooooooooooooooooooooooooooooooooooooooooooo\n";
-    } else {
-        cout << "I hit the snake yassssssssssssssssssssssssssssssssssssssss.\n";
-        health -= 10;
-    }
-    cnt++;
-    if (health == 0){
-        cout<<"Dah snek is ded\n";
-        cout << "Took you " << cnt << " hits ya loser. Get better dexterity.";
-        break;
-    }
-    }
+	default_random_engine randomGenerator(time(NULL));
+	uniform_real_distribution<float> atticRoll(0.0f, 1.0f);
+	int health = 100;
+	int cnt = 0;
+	while (true) {
+		float attack = atticRoll(randomGenerator);
+		if (attack > 0.3f) {
+			cout << "I misseed the snake oh noooooooooooooooooooooooooooooooooooooooooooo\n";
+		}
+		else {
+			cout << "I hit the snake yassssssssssssssssssssssssssssssssssssssss.\n";
+			health -= 10;
+		}
+		cnt++;
+		if (health == 0) {
+			cout << "Dah snek is ded\n";
+			cout << "Took you " << cnt << " hits ya loser. Get better dexterity.";
+			break;
+		}
+	}
 
 }
 
